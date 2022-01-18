0ecd0c77c3311fbee8d507e44a2d5ad59c692c6f
maheshvakkund3107 <maheshvakkund3107@Outlook.com>
2022-01-18 23:48:48 +0530

    Infix Patterns,Decomposing Sequences.Custom return types for unapply in scala
    Infix Patterns,Decomposing Sequences.Custom return types for unapply in scala

--- src/main/scala/AdvancedPatternMatching.scala
+++ src/main/scala/AdvancedPatternMatching.scala
@@ -66,4 +66,62 @@ object AdvancedPatternMatching extends App {
   }
   println(mathProperty)
 
+  /** Infix Patterns.
+    */
+
+  case class Or[A, B](a: A, b: B)
+  val either = Or(2, "two")
+  val humanDesc: String = either match {
+    case number Or string => s"$number is written as $string"
+  }
+  println(humanDesc)
+
+  /** Decomposing Sequences
+    */
+
+  val vararg = numbers match {
+    case List(1, _*) => "starting with 1"
+  }
+
+  abstract class MyList[+A] {
+    def head: A = ???
+    def tail: MyList[A] = ???
+  }
+
+  case object Empty extends MyList[Nothing]
+  case class Cons[+A](override val head: A, override val tail: MyList[A])
+      extends MyList[A]
+  object MyList {
+    def unapplySeq[A](list: MyList[A]): Option[Seq[A]] = {
+      if (list == Empty) Some(Seq.empty)
+      else unapplySeq(list.tail).map(list.head +: _)
+    }
+  }
+  val myList: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
+  val decomposed = myList match {
+    case MyList(1, 2, _*) => "Starting with 1,2"
+    case _                => "Something else"
+  }
+  println(decomposed)
+
+  /** Custom return types for unapply
+    * isEmpty: Boolean, get :Something
+    */
+
+  abstract class Wrapper[T] {
+    def isEmpty: Boolean
+    def get: T
+  }
+
+  object PersonWrapper {
+    def unapply(person: Person): Wrapper[String] = new Wrapper[String] {
+      override def isEmpty: Boolean = false
+      override def get: String = person.name
+    }
+  }
+
+  println(bob match {
+    case PersonWrapper(n) => s"This persons name is $n"
+    case _                => "An Alien"
+  })
 }
